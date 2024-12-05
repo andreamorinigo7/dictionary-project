061e692d1113342786f919ebfcebfd9b1dd76078
Andrea <morinigoandrea7@gmail.com>
2024-12-05 18:19:33 -0500

    search engine running

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Dictionary</h1>
+        <Dictionary />
       </header>
     </div>
   );
