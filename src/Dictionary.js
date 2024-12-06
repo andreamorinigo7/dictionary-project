cde07ba7f48d9fbaa78987dd898c07c3369ecfde
Andrea <morinigoandrea7@gmail.com>
2024-12-05 21:08:11 -0500

    added API, functional

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
-
+import axios from "axios";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(" ");
 
+  function handleResponse(response) {
+    console.log(response.data.meanings[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`${keyword}`);
+
+    let apiKey = "o7aa001199a3fa308a4b424t253e2953";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
