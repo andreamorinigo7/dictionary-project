c7710dd37d12c2128daf8d23f2d850886f7b3332
Andrea <morinigoandrea7@gmail.com>
2024-12-07 17:01:30 -0500

    added definition css structure

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,26 @@
 import React from "react";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
+      <div className="text-container">
+        <span>
+          <h3>{props.meaning.partOfSpeech}</h3>
+        </span>
+        <span className="dashed-line"></span>
+      </div>
+      <div className="def-meaning">meaning</div>
       <div className="definition">
         <p>{props.meaning.definition}</p>
       </div>
-      <br />
+
       <div className="example">
         {" "}
-        <em> {props.meaning.example}</em>
+        <p>
+          <em> {props.meaning.example}</em>
+        </p>
       </div>
       <br />
     </div>
