322f6751d6103c00082e3b865435b981d12ef7e6
Andrea <morinigoandrea7@gmail.com>
2024-12-07 18:49:43 -0500

    debug failed build

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
 import "./Results.css";
-import Synonyms from "./Synonyms";
 
 export default function Results(props) {
   if (props.results) {
